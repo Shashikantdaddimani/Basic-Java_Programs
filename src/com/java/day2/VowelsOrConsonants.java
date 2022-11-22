7925b9b12e3fdb97eb69484c931712840e8a44c2
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-22 08:01:44 +0530

    Added:Vowels or consanants while using switch case

---
+++ src/com/java/day2/VowelsOrConsonants.java
@@ -0,0 +1,50 @@
+package com.java.day2;
+
+import java.util.Scanner;
+
+public class VowelsOrConsonants {
+
+	public static void main(String[] args) {
+		System.out.println("Enter The Alpabates");
+	Scanner Sc = new Scanner(System.in);
+	char N=Sc.next().charAt(0);
+	
+	switch(N) {
+	case 'A' : 
+		System.out.println("Is Vowel");
+		break;
+	case 'E':
+		System.out.println("Is Vowel");
+		break;
+	case 'I':
+		System.out.println("Is Vowel");
+		break;
+	case 'O':
+		System.out.println("Is vowel");
+		break;
+	case 'U':
+		System.out.println("is Vowel");
+		break;
+	case 'a':
+		System.out.println("Is Vowel");
+		break;
+	case 'e':
+		System.out.println("Is Vowel");
+		break;
+	case 'i':
+		System.out.println("is Vowel");
+		break;
+	case 'o':
+		System.out.println("Is Vowel");
+		break;
+	case 'u':
+		System.out.println("Is Vowel");
+		break;
+	default :
+		System.out.println("is Consanant");
+		
+	}
+
+	}
+
+}
