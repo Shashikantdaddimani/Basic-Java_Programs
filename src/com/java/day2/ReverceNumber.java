52d5051e749eb673e145cd53cf3bf80d316989bb
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-22 23:12:46 +0530

    Added logic to find reverse number by using while loop

--- src/com/java/day2/ReverceNumber.java
+++ src/com/java/day2/ReverceNumber.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class ReverceNumber {
 
 	public static void main(String[] args) {
+		//Taking user input
 		Scanner Sc = new Scanner(System.in);
 		System.out.println("Enter the Number");
 		int number = Sc.nextInt();
 		int remainder;
-		int reverce=0;
+		int reverse=0;
 		
 		while(number!=0)
 		{
 			remainder= number%10;
-			reverce=reverce*10+remainder;
+			reverse=reverse*10+remainder;
 			number=number/10;
 			
 		}
-		System.out.println("The reverse of the given number is: " +  reverce); 
+		System.out.println("The reverse of the given number is: " +  reverse); 
 	}
 
 }
