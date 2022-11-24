822b5105095e4df841e2d3b8411ad919ebfbbf91
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 14:48:47 +0530

    Added logic to find Vowels or consanants using switch cases

--- src/com/java/day2/VowelsOrConsonants.java
+++ src/com/java/day2/VowelsOrConsonants.java
@@ -1,16 +1,25 @@
+/**
+ * Added logic to find Vowels or consanants using switch cases
+ */
 package com.java.day2;
 
+//imports the scanner class of the java.utill package
 import java.util.Scanner;
 
-public class VowelsOrConsonants {
-
-	public static void main(String[] args) {
+//class definition
+public class VowelsOrConsonants 
+{
+	//main method
+	public static void main(String[] args) 
+	{
 		//Taking user input 
-		System.out.println("Enter The Alpabates");
-	Scanner Sc = new Scanner(System.in);
-	char Char=Sc.next().charAt(0);
+	System.out.println("Enter The Alpabates");
+	Scanner input = new Scanner(System.in);
+	char Char=input.next().charAt(0);
 	
-	switch(Char) {
+	//logic to finding vowel or consonants
+	switch(Char) 
+	{
 	case 'A' : 
 		System.out.println("Is Vowel");
 		break;
