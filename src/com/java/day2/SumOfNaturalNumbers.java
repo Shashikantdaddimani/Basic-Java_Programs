1c288e2c88802915a3068911d80301f067664885
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 14:36:41 +0530

    Added logic to find the Sum of natural numbers using while loop

--- src/com/java/day2/SumOfNaturalNumbers.java
+++ src/com/java/day2/SumOfNaturalNumbers.java
@@ -1,15 +1,23 @@
+/**
+ * Write a Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+ */
 package com.java.day2;
 
+//imports the scanner class of the java.utill package
 import java.util.Scanner;
 
-public class SumOfNaturalNumbers {
-
-	public static void main(String[] args) {
+//class definition
+public class SumOfNaturalNumbers 
+{
+	//main method
+	public static void main(String[] args) 
+	{
 		//Taking user input
-		Scanner sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Natural Number:");
-		int number = sc.nextInt();
+		int number = input.nextInt();
 		int sum=0;
+		//logic to find sum of natural number
 		while(number != 0)
 		{
 			sum = sum + number;
