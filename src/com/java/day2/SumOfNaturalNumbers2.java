06db6171033e65c832271492a74a6c14da5b73bd
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 14:42:39 +0530

    Added logic to find the Sum of natural numbers using for loop

--- src/com/java/day2/SumOfNaturalNumbers2.java
+++ src/com/java/day2/SumOfNaturalNumbers2.java
@@ -1,27 +1,35 @@
+/**
+ * Added logic to find the Sum of natural numbers using for loop
+ */
 package com.java.day2;
 
+//imports the scanner class of the java.utill package
 import java.util.Scanner;
 
-public class SumOfNaturalNumbers2 {
-
-	public static void main(String[] args) {
+//class definition
+public class SumOfNaturalNumbers2 
+{
+	//main method
+	public static void main(String[] args) 
+	{
 		//Taking user input
-		Scanner Sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a number: ");
-		int number= Sc.nextInt(); 
+		int number= input.nextInt(); 
 		int sum = 0;
 				
 		System.out.print("First " + number + " Numbers = ");
-		for (int i=1;i<=number;i++) {
+		for (int i=1;i<=number;i++) 
+		{
 			sum=sum+i;
 			
 			System.out.print(i + " + ");
 
-	}
+	    }
 		System.out.println();
 	System.out.println("Sum of natural numbers using for loop is:"+" "+sum);
 
-      }
+     }
 
 }
 
