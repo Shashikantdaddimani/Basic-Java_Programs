c8876b99affc7eca6c159b67452dc220f1decd19
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 14:24:55 +0530

    Added logic to find a taken two int values from the command line and print true or false

--- src/com/java/day2/SpringSeasson.java
+++ src/com/java/day2/SpringSeasson.java
@@ -1,15 +1,24 @@
+/**
+ * Write a program SpringSeason.java that takes two int values m and d from the command line and prints 
+ * true if day d of month m is between March 20 (m = 3, d=20) and June 20 (m = 6, d = 20), false otherwise.
+ */
 package com.java.day2;
 
+//imports the scanner class of the java.utill package
 import java.util.Scanner;
 
-public class SpringSeasson {
+//class definition
+public class SpringSeasson 
+{
+	//main method
 	public static void main(String[] args) {
 		//Taking user input
-		Scanner Sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Month: ");
-		int month = Sc.nextInt();
+		int month = input.nextInt();
 		System.out.println("Enter Date: ");
-		int date = Sc.nextInt();
+		int date = input.nextInt();
+		
 		if(date<=20)
 		{
 			for(int i=1; i<=date; i++)
