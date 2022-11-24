b0bc7a5277c43192d218d6042e3a81dc4edf0804
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-24 13:17:57 +0530

    Added logic for calculating some equations using Arithmetic operators and finding min and max values

--- src/com/java/day2/AirthmaticOperation.java
+++ src/com/java/day2/AirthmaticOperation.java
@@ -1,16 +1,26 @@
+/**
+ * Enter two numbers and do the following arithmetic Operations find max and min.
+      i) a+b*c ii) c+a/b
+      iii) a%b+c iV) a*b+c
+ */
 package com.java.day2;
 
+//imports the scanner class of the java.utill package
 import java.util.Scanner;
 
+//class definition
 public class AirthmaticOperation {
+	//main method
 	public static void main(String[] args) {
+		
 		// Taking user input
-		Scanner sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the 3 number");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		// calculating values
+		int a = input.nextInt();
+		int b = input.nextInt();
+		int c = input.nextInt();
+		
+		// calculating values using given equation
 		System.out.println("calculating a+b*c: ");
 		int result1 = a+b*c;
 		System.out.println("result: "+result1);
@@ -27,6 +37,8 @@ public class AirthmaticOperation {
 		int result4 = a*b+c;
 		System.out.println("result: "+result4);
 		
+		//logic to find the greater or smaller
+		
 		if((result1<result2) && (result1<result3) && (result1<result4))
 			System.out.println(result1+" is smaller");
 		else if((result2<result3) && (result2<result4)) 
@@ -37,13 +49,19 @@ public class AirthmaticOperation {
 			System.out.println(result4+" is smaller");
 		
 		
-		if((result1>result2) && (result1>result3) && (result1>result4)){
+		if((result1>result2) && (result1>result3) && (result1>result4))
+		{
 			System.out.println(result1+" is Greater");
-		}else if((result2>result3) && (result2<result4)) {
+		}
+		else if((result2>result3) && (result2<result4)) 
+		{
 			System.out.println(result2+" is Greater");
-		}else if((result3>result4)) {
+		}
+		else if((result3>result4)) {
 			System.out.println(result3+" is Greater");
-		}else {
+		}
+		else 
+		{
 			System.out.println(result4+" is Greater");
 		}
 		
